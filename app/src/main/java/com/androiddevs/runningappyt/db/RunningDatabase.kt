2d12d13f39d307efbc23125839828138d4ad3642
Jahid <jahidul479@gmail.com>
2021-06-29 20:11:56 +0600

    added converter and database

---
+++ app/src/main/java/com/androiddevs/runningappyt/db/RunningDatabase.kt
@@ -0,0 +1,17 @@
+package com.androiddevs.runningappyt.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+
+@Database(
+    entities = [
+        Run::class
+    ],
+    version = 1
+)
+@TypeConverters(Converters::class)
+abstract class RunningDatabase : RoomDatabase() {
+
+    abstract fun getRunDao(): RunDao
+}
